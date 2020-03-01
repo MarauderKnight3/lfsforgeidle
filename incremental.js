d58bd4e2588b775c986e819d364f7e6615540e65
MarauderKnight3 <48040746+MarauderKnight3@users.noreply.github.com>
2020-02-29 17:00:31 -0700

    Update incremental.js

--- incremental.js
+++ incremental.js
@@ -1,129 +1,197 @@
-var iteration = window.setInterval(function () {
-    tick()
-}, 40);
-var money = 0;
-var earnedSoFar = 0;
-var entireCPS = 0;
-
-//To add another building, you MUST add another building data set into the buildings array with its stats. This can simply be done by copying an object {...} below and editing the stats. See the JSON to add in said building's place in the table on the site.
-var buildings = [{
-        name: "Novice Forge",
+window.setInterval(function() {
+    tick();
+}, 25);
+var game = {
+    money: 0,
+    earnedSoFar: 0,
+    entireCPS: 0,
+    coinsOnClick: 1,
+    buildings: [{
+        name: "Wood Fire",
         CPS: 0.2,
         ESF: 0,
         owned: 0,
-        cost: 10
+        cost: 16
     }, {
-        name: "Adept Forge",
-        CPS: 3,
+        name: "Anvil",
+        CPS: 1,
         ESF: 0,
         owned: 0,
+        cost: 140
+    }, {
+        name: "Smelter",
+        CPS: 8,
+        ESF: 0,
+        owned: 0,
+        cost: 1150
+    }, {
+        name: "Bellows",
+        CPS: 14,
+        ESF: 0,
+        owned: 0,
+        cost: 3700
+    }, {
+        name: "Clay Furnace",
+        CPS: 24,
+        ESF: 0,
+        owned: 0,
+        cost: 12000
+    }, {
+        name: "Well",
+        CPS: 45,
+        ESF: 0,
+        owned: 0,
+        cost: 21000
+    }, {
+        name: "Smithy",
+        CPS: 96,
+        ESF: 0,
+        owned: 0,
+        cost: 155000
+    }, {
+        name: "Trading center",
+        CPS: 130,
+        ESF: 0,
+        owned: 0,
+        cost: 940340
+    }, {
+        name: "Market",
+        CPS: 300,
+        ESF: 0,
+        owned: 0,
+        cost: 1690000
+    }, {
+        name: "Canned Beans",
+        CPS: 1000,
+        ESF: 0,
+        owned: 0,
+        cost: 12000000
+    }],
+    upgrades: [{
+        name: "Iron Dagger",
+        desc: "There's no such thing as a blacksmith who hasn't made too many daggers.",
+        prerequisiteDesc: "For each except first: need 2 \"" + game.buildings[1].name + "\"",
+        addToClick: 0,
+        multToClick: 0.02,
+        addToCPS: 0,
+        multToCPS: 0.02,
+        discountBuilds: 0,
+        owned: 0,
+        limit: -1,
         cost: 100,
-    }, {
-        name: "Great Forge",
-        CPS: 12,
-        ESF: 0,
-        owned: 0,
-        cost: 220
-    }, {
-        name: "Diehard Forge",
-        CPS: 30,
-        ESF: 0,
-        owned: 0,
-        cost: 590
+        prerequisite: function() {
+            var stat = "";
+            if (this.owned !== 0) {
+                stat = game.buildings[1].owned + "/" + this.owned * 2 + " needed";
+            }
+            return [game.buildings[1].owned >= this.owned * 2, stat];
+        }
+    }],
+    toTenth: function(num = 0) {
+        return parseFloat(Math.round(num * 100) / 100);
+    },
+    calcBuildingCost: function(ID, speculateOwned = game.buildings[ID].owned) {
+        return game.buildings[ID].cost + Math.ceil(0.15 * Math.pow(speculateOwned, 2) * game.buildings[ID].cost * 0.125);
+    },
+    calcUpgradeCost: function(ID, speculateOwned = game.upgrades[ID].owned) {
+        return game.buildings[ID].cost + Math.ceil(0.12 * Math.pow(speculateOwned, 2) * game.buildings[ID].cost * 0.4);
     }
-];
-
-function correctRound(num) {
-    return parseFloat(Math.round(num * 100) / 100);
-}
-
-function updateDisplay() {
-    document.getElementById("money").innerHTML = Math.round(money);
-    document.getElementById("earnedSoFar").innerHTML = Math.round(earnedSoFar);
-    document.getElementById("entireCPS").innerHTML = correctRound(entireCPS);
-    for (i = 0; i <= buildings.length - 1; i++) {
-        document.getElementById(i + "name").innerHTML = buildings[i].name;
-        document.getElementById(i + "cost").innerHTML = "(Costs " + findAmountCanBuy(parseInt(i), getAmountToBuy(), true)[1] + " for " + findAmountCanBuy(parseInt(i), getAmountToBuy(), true)[0] + ")";
-        document.getElementById(i + "owned").innerHTML = buildings[i].owned;
-        document.getElementById(i + "CPS").innerHTML = buildings[i].CPS;
-        document.getElementById(i + "totalCPS").innerHTML = correctRound(buildings[i].CPS * buildings[i].owned);
-        document.getElementById(i + "ESF").innerHTML = Math.round(buildings[i].ESF);
-    }
-}
-
-function findCost(ID) {
-    return buildings[ID].cost + Math.ceil((0.15 * Math.pow(buildings[ID].owned, 2)) * (buildings[ID].cost / 8));
 }
 
 function getAmountToBuy() {
     if (!document.getElementById("amountToBuy").value) {
         return 1;
     }
-    if (isNaN(parseInt(document.getElementById("amountToBuy").value))) {
-        return 1;
-    } else {
-        return parseInt(document.getElementById("amountToBuy").value)
+    if (document.getElementById("amountToBuy").value < 1) {
+        document.getElementById("amountToBuy").value = 1;
     }
+    return document.getElementById("amountToBuy").value;
 }
 
-function findAmountCanBuy(ID, amount, would, andBuy = false) {
-    var amountSoFar = 0;
-    var speculateOwned = buildings[ID].owned;
-    var monie = Math.round(money);
+function buildingsCanBuy(ID, amount = getAmountToBuy(), would = false) {
+    var speculateCanBuy = 0;
+    var speculateOwned = game.buildings[ID].owned;
     var totalCost = 0;
     if (!would) {
-        for (j = amount; j > 0; j--) {
-            if (monie - (buildings[ID].cost + Math.ceil((0.15 * Math.pow(speculateOwned, 2)) * (buildings[ID].cost / 8))) >= 0) {
-                monie -= buildings[ID].cost + Math.ceil((0.15 * Math.pow(speculateOwned, 2)) * (buildings[ID].cost / 8));
-                totalCost += buildings[ID].cost + Math.ceil((0.15 * Math.pow(speculateOwned, 2)) * (buildings[ID].cost / 8));
+        var availableMoney = Math.round(game.money);
+        for (var j = amount; j !== 0; j--) {
+            if (availableMoney - game.calcBuildingCost(ID, speculateOwned) >= 0) {
+                availableMoney -= game.calcBuildingCost(ID, speculateOwned);
+                totalCost += game.calcBuildingCost(ID, speculateOwned);
                 speculateOwned++;
-                amountSoFar++;
-            } else {
+                speculateCanBuy++;
+            }
+            else {
                 break;
             }
         }
-    } else {
-        for (j = amount; j > 0; j--) {
-            totalCost += buildings[ID].cost + Math.ceil((0.15 * Math.pow(speculateOwned, 2)) * (buildings[ID].cost / 8));
+    }
+    else {
+        for (var j = amount; j !== 0; j--) {
+            totalCost += game.calcBuildingCost(ID, speculateOwned);
             speculateOwned++;
-            amountSoFar++;
+            speculateCanBuy++;
         }
     }
-    if (!would && andBuy) {
-        money -= totalCost;
-        buildings[ID].owned += amountSoFar;
+    return [speculateCanBuy, totalCost];
+}
+
+function updateDisplay() {
+    document.getElementById("money").textContent = Math.round(game.money);
+    document.getElementById("earnedSoFar").textContent = Math.round(game.earnedSoFar);
+    document.getElementById("entireCPS").textContent = correctRound(game.entireCPS);
+    for (var j = 0; j <= buildings.length - 1; j += 1) {
+        document.getElementById(j + "name").textContent = game.buildings[j].name;
+        document.getElementById(j + "cost").textContent = buildingsCanBuy(j, getAmountToBuy(), true)[1] + " (For " + buildingsCanBuy(j, getAmountToBuy(), true)[0] + ")";
+        document.getElementById(j + "owned").textContent = game.buildings[j].owned;
+        document.getElementById(j + "CPS").textContent = game.buildings[j].CPS;
+        document.getElementById(j + "totalCPS").textContent = game.toTenth(game.buildings[j].CPS * game.buildings[j].owned);
+        document.getElementById(j + "ESF").textContent = Math.round(game.buildings[j].ESF);
     }
-    return [amountSoFar, totalCost];
 }
 
 function initialize() {
-    money = 0;
-    updateDisplay();
+    var coll = document.getElementsByClassName("collapsible");
+    for (var i = 0; i < coll.length; i += 1) {
+        coll[i].addEventListener("click", function() {
+            classList.toggle("active");
+            var content = nextElementSibling;
+            if (content.style.display == "block") {
+                content.style.display = "none";
+            }
+            else {
+                content.style.display = "block";
+            }
+        });
+    }
 }
 
 function tick() {
-    entireCPS = 0;
-    for (i = 0; i < buildings.length; i++) {
-        money += buildings[i].owned * buildings[i].CPS / 25;
-        earnedSoFar += buildings[i].owned * buildings[i].CPS / 25;
-        entireCPS += buildings[i].owned * buildings[i].CPS;
-        buildings[i].ESF += buildings[i].owned * buildings[i].CPS / 25;
+    game.entireCPS = 0;
+    for (var i = 0; i < game.buildings.length; i += 1) {
+        game.money += game.buildings[i].owned * game.buildings[i].CPS / 40;
+        earnedSoFar += game.buildings[i].owned * game.buildings[i].CPS / 40;
+        buildings[i].ESF += game.buildings[i].owned * game.buildings[i].CPS / 40;
+        entireCPS += game.buildings[i].owned * game.buildings[i].CPS;
     }
-    if (getAmountToBuy() > 100000)
-        document.getElementById("amountToBuy").value = "1000";
+    if (getAmountToBuy() > 1000000000000) {
+        document.getElementById("amountToBuy").value = "1000000000000";
+    }
     updateDisplay();
 }
 
 function gatherMoney() {
-    money += 1;
-    earnedSoFar += 1;
+    game.money += game.coinsOnClick;
+    earnedSoFar += game.coinsOnClick;
     updateDisplay();
 }
 
-function buyAcquisition(ID) {
-    if (getAmountToBuy(ID, getAmountToBuy()) == 1) {
-        document.getElementById("amountToBuy").value = "1";
+function buyBuilding(ID) {
+    if (getAmountToBuy() == 1) {
+        document.getElementById("amountToBuy").value = 1;
     }
-    findAmountCanBuy(ID, getAmountToBuy(), false, true);
+    var k = buildingsCanBuy(ID);
+    money -= k[1];
+    buildings[ID].owned += k[0];
     updateDisplay();
 }
